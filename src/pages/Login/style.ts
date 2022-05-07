c3bb0ad8ddefe5c619ae635ae3b39aee0f0735f9
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-07 15:56:13 -0300

    add style for login page

---
+++ src/pages/Login/style.ts
@@ -0,0 +1,14 @@
+import { css } from '@emotion/css';
+
+export const animation = css`
+  width: 200px;
+  margin-bottom: 40px;
+`;
+
+export const marginFormElements = css`
+  margin: 20px;
+`;
+
+export const textStyled = css`
+  font-size: 10pt;
+`
