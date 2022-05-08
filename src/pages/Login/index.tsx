9654b28cae4d03e4868cbbfa2849eb2e54fe0f98
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-08 08:36:59 -0300

    add component to form login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,27 +1,16 @@
-import React from 'react';
-import { animation, marginFormElements, textStyled } from './style'
-import { Button, FormLabel, Input, FormErrorMessage, FormControl } from '@chakra-ui/react'
-import { useForm } from 'react-hook-form'
+import React, { useEffect } from 'react';
+import { animation, textStyled } from './style'
 import Lottie from 'react-lottie-player';
 
 import animationPlant from '../../assets/animations/plant.json'
 import { Container } from '../../components/Container';
-import { login } from './service';
-import { useNavigate } from 'react-router-dom';
-
-interface IFormInput {
-  email: string;
-  password: string;
-}
+import FormLogin from './FormLogin';
+import { logout } from './service';
 
 const Login = () => {
-  const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors, isValid, isSubmitted } } = useForm<IFormInput>()
-
-
-  const onSubmit = (data: IFormInput) => {
-    login(data.email, data.password, navigate)
-  };
+  useEffect(() => {
+    logout();
+  }, [])
 
   return (
     <>
@@ -36,45 +25,7 @@ const Login = () => {
         <p className={textStyled}>Faça login para continuar</p>
 
         <Container width='400px' height='300px'>
-          <form
-            onSubmit={
-              handleSubmit(onSubmit)
-            }
-          >
-            <FormControl isInvalid={isSubmitted && !isValid}>
-              <Container>
-                <FormLabel htmlFor='email' className={marginFormElements}>
-                  Email
-                  <Input
-                    id='email'
-                    type='email'
-                    {...register('email', {
-                      required: "Digite o email",
-                      pattern: {
-                        value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                        message: "Email inválido"
-                      }
-                    })}
-                  />
-                  {errors?.email?.message && <FormErrorMessage>{errors.email.message}</FormErrorMessage>}
-                </FormLabel>
-
-                <FormLabel htmlFor='password' className={marginFormElements}>
-                  Senha
-                  <Input
-                    id='password'
-                    type='password'
-                    {...register('password', { required: 'Digite a senha' })}
-                  />
-                  {errors?.password?.message && <FormErrorMessage>{errors.password.message}</FormErrorMessage>}
-                </FormLabel>
-
-                <Button id='submit' type='submit' colorScheme='teal' className={marginFormElements}>
-                  Entrar
-                </Button>
-              </Container>
-            </FormControl>
-          </form>
+          <FormLogin />
         </Container>
       </Container>
     </>
