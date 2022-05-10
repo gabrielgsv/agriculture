db0f0cdb7c2bd4392d08cedac4c4a41efc74d7ea
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-10 17:48:27 -0300

    add actions to getProducers, getEdit and cleanEdit

---
+++ src/store/actions/producer.ts
@@ -0,0 +1,45 @@
+import api from "../../services/api"
+import { DispatchGetEditType, DispatchType } from "../types/producer"
+
+export function getProducers() {
+  return function (dispatch: DispatchType) {
+    api
+      .get(`producer`)
+      .then(res => {
+        const result = res.data
+        dispatch({
+          type: 'GET_PRODUCER',
+          producers: result,
+        })
+      })
+      .catch((err) => {
+        throw err
+      })
+  }
+}
+
+export function getEdit(idEdit: string) {
+  return function (dispatch: DispatchGetEditType) {
+    api
+      .get(`producer?id=${idEdit}`)
+      .then(res => {
+        const result = res.data
+        dispatch({
+          type: 'GET_EDIT',
+          editProducer: result,
+        })
+      })
+      .catch((err) => {
+        throw err
+      })
+  }
+}
+
+
+export function cleanEdit() {
+  return function (dispatch: any) {
+    dispatch({
+      type: 'CLEAN_EDIT',
+    })
+  }
+}
