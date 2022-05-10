ebbf5788f44443497233f9eca23c0e9df2a908f7
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-10 17:50:33 -0300

    ad grid css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import "gridjs/dist/theme/mermaid.css";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
