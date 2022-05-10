f7e2b4d76ffc9437f95bd8b31d727d2d6071dc79
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-10 17:47:47 -0300

    add grid and update modalForm

--- src/pages/Producer/index.tsx
+++ src/pages/Producer/index.tsx
@@ -1,12 +1,32 @@
+import { useDisclosure } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 import { Container } from "../../components/Layout/Container";
 import { Title } from "../../components/Layout/Title";
+import Grid from "./Grid";
 import ModalForm from "./ModalForm";
 
 const Producer = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const editProducer: any = useSelector(
+    (state: any) => {
+      return state.Producer?.editProducer
+    }
+  )
+
+  useEffect(() => {
+    if (editProducer) {
+      onOpen();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editProducer])
+
   return (
     <Container width="100%">
       <Title>Produtor Rural</Title>
-      <ModalForm />
+      <ModalForm isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+      <Grid />
     </Container>
   );
 };
