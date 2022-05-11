7efa979d6fd41e2166312d2e2792d266994f90a0
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-11 14:56:10 -0300

    update react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import "gridjs/dist/theme/mermaid.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root');
+const root = createRoot(container!);
 root.render(<App />);
