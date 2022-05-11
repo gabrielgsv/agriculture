d75570ed25d0a55bfb33b421431b64c1023e7a3e
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-11 17:48:26 -0300

    add TotalCounts and get data in redux action

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { Dispatch, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Title } from "../../components/Layout/Title";
+import { getDashboard } from "../../store/actions/dashboard";
+import TotalCounts from "./TotalCounts";
 
 const Dashboard = () => {
+  const dispatch: Dispatch<any> = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDashboard());
+  }, []);
+
   return (
     <>
-      <h1>Dashboard</h1>
+      <Title>Dashboard</Title>
+      <TotalCounts />
     </>
   );
 };
