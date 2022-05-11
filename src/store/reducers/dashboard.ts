9fca9e3a2217f931afc99d7404f0c0ed538a73a9
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-11 17:49:31 -0300

    reducer to get dashboardData

---
+++ src/store/reducers/dashboard.ts
@@ -0,0 +1,19 @@
+import { DashboardAction, DashboardState } from "../types/dashboard"
+
+const dashboard = (
+  state: any = {},
+  action: DashboardAction
+): DashboardState => {
+  switch (action.type) {
+    case 'GET_DASHBOARD':
+      return {
+        ...state,
+        dashboardData: action.dashboardData,
+        totalFarms: action.totalFarms,
+      }
+    default:
+      return state
+  }
+}
+
+export default dashboard
