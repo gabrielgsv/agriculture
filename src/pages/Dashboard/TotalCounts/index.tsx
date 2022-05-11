2e23d1d32ccb52686fa7fd4c4955cf90f1f430cc
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-11 17:48:02 -0300

    show totalFarm and totalHectares

---
+++ src/pages/Dashboard/TotalCounts/index.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Container } from "../../../components/Layout/Container";
+import { DashboardState } from "../../../store/types/dashboard";
+import { getTotalArea } from "../services";
+import Count from "./Count";
+
+const TotalCounts = () => {
+  const [totalFarms, setTotalFarms] = useState(0);
+  const [totalArea, setTotalArea] = useState(0);
+
+  const selector: DashboardState = useSelector((state: any) => state.Dashboard);
+
+  useEffect(() => {
+    if (selector) {
+      const area = getTotalArea(selector.dashboardData);
+      setTotalArea(area?.totalHectares);
+      setTotalFarms(selector.totalFarms);
+    }
+  }, [selector])
+
+  return (
+    <Container row={true}>
+      <Count name="Total de fazendas:" number={totalFarms} />
+      <Count name="Total de área em hectares:" number={totalArea} />
+    </Container>
+  );
+};
+
+export default TotalCounts;
