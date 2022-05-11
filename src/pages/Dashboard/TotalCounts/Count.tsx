4c9a5170489ac1d40187ef576d61f74927619909
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-11 17:47:23 -0300

    add count component

---
+++ src/pages/Dashboard/TotalCounts/Count.tsx
@@ -0,0 +1,19 @@
+import { Text } from '@chakra-ui/react';
+import { Card } from '../../../components/Layout/Card';
+
+interface IProps {
+  name: string
+  number: number
+}
+
+const Count = ({ name, number }: IProps) => {
+
+  return (
+    <Card width='300px' height="200px" column={true} margin='20px'>
+      <p>{name}</p>
+      <Text fontSize="40pt">{number}</Text>
+    </Card>
+  );
+}
+
+export default Count;
