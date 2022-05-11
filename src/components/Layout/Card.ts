46a7b35cf703a3b11515ace2bba9bed8e0da119e
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-11 17:46:52 -0300

    fix style card

--- src/components/Layout/Card.ts
+++ src/components/Layout/Card.ts
@@ -5,6 +5,7 @@ interface Props {
   width: string;
   height: string;
   column?: boolean;
+  margin?: string;
 }
 
 export const Card = styled(Container) <Partial<Props>>`
@@ -12,4 +13,7 @@ export const Card = styled(Container) <Partial<Props>>`
   flex-direction: ${props => props.column ? "column" : "row"};
   width: ${props => props.width};
   height: ${props => props.height};
+  border: 1px solid #e2e8f0;
+  border-radius: 10px;
+  margin: ${props => props.margin};
 `;
