38f3aeb92ce933a70592b074dc6b0e938e2126e5
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-12 11:47:37 -0300

    add pieCharts

--- src/pages/Dashboard/PieCharts/index.tsx
+++ src/pages/Dashboard/PieCharts/index.tsx
@@ -1,51 +1,21 @@
-import { Card } from "../../../components/Layout/Card";
-import { Box } from "@chakra-ui/react";
-import { DashboardState } from "../../../store/types/dashboard";
-import { useSelector } from "react-redux";
-import { getUfData } from "./services";
-import { Pie } from "@ant-design/plots";
+import { DashboardState } from '../../../store/types/dashboard';
+import { useSelector } from 'react-redux';
+import { getAreaData, getCropsData, getUfData } from './services';
+import Chart from './Chart';
+import { Flex } from '@chakra-ui/react';
 
 const PieCharts = () => {
   const selector: DashboardState = useSelector((state: any) => state.Dashboard);
   const ufData: any = selector?.dashboardData && getUfData(selector.dashboardData)
-
-  console.log('ufData', ufData)
-
-  const config = {
-    appendPadding: 10,
-    data: ufData || [],
-    angleField: 'value',
-    colorField: 'type',
-    radius: 0.9,
-    autoFit: true,
-    label: {
-      type: 'inner',
-      offset: '-30%',
-      content: '{percentage}',
-      style: {
-        fontSize: 9,
-        textAlign: 'center',
-      },
-    },
-    interactions: [
-      {
-        type: 'element-selected',
-      },
-      {
-        type: 'element-active',
-      },
-    ],
-  };
+  const cropsData: any = selector?.dashboardData && getCropsData(selector.dashboardData)
+  const areaData: any = selector?.dashboardData && getAreaData(selector.dashboardData)
 
   return (
-    <>
-      <Card minWidth="300px" column={true} margin='20px'>
-        <p>Gráfico por estado(UF)</p>
-        <Box width='90%'>
-          <Pie {...config} />
-        </Box>
-      </Card>
-    </>
+    <Flex flexFlow='row wrap' justify='center' width='100%'>
+      <Chart name='Estado(UF):' data={ufData} />
+      <Chart name='Cultura:' data={cropsData} />
+      <Chart name='Uso do Solo:' data={areaData} />
+    </Flex>
   );
 };
 
--- src/pages/Dashboard/PieCharts/services.ts
+++ src/pages/Dashboard/PieCharts/services.ts
@@ -1,3 +1,5 @@
+import i18next from 'i18next';
+
 export function getUfData(data: any) {
   if (data) {
     const ufDataCount = data.reduce((previus: any, current: any) => {
@@ -16,8 +18,48 @@ export function getUfData(data: any) {
       };
     });
 
-    console.log('ufDataArray', ufDataArray);
-
     return ufDataArray;
+  }
+}
+
+export function getCropsData(data: any) {
+  if (data) {
+    const cropsDataCount = data.reduce((previus: any, current: any) => {
+      let cropsPlantedGroup = current.cropsPlanted;
+      cropsPlantedGroup.forEach((cropsPlanted: any) => {
+        if (!previus.hasOwnProperty(cropsPlanted)) {
+          previus[cropsPlanted] = 0;
+        }
+        previus[cropsPlanted]++;
+      })
+      return previus;
+    }, {});
+
+
+    const cropsPlantedArray = Object.keys(cropsDataCount).map((key: any) => {
+      return {
+        type: i18next.t(key),
+        value: cropsDataCount[key]
+      };
+    });
+
+    return cropsPlantedArray;
+  }
+}
+
+export function getAreaData(data: any) {
+  if (data) {
+    const arableHectares = data.reduce((previus: any, current: any) => {
+      return previus + Number(current.arableHectares);
+    }, 0);
+
+    const vegetationArable = data.reduce((previus: any, current: any) => {
+      return previus + Number(current.vegetationArable);
+    }, 0);
+
+    return [
+      { type: 'Área agricultável', value: arableHectares },
+      { type: 'Área de vegetação', value: vegetationArable }
+    ];
   }
 }
