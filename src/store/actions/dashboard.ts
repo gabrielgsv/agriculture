b3821845f38e083280286660137c7a59cad49758
Gabriel Soares Vilela <gabriel.soares.vilela@gmail.com>
2022-05-12 22:57:55 -0300

    fix get all datas

--- src/store/actions/dashboard.ts
+++ src/store/actions/dashboard.ts
@@ -2,12 +2,12 @@ import api from '../../services/api';
 
 export function getDashboard() {
   return function (dispatch: any) {
-    api.get(`/producer?_page=1`)
+    api.get(`/producer`)
       .then(res => {
         dispatch({
           type: 'GET_DASHBOARD',
           dashboardData: res.data,
-          totalFarms: Number(res.headers['x-total-count'])
+          totalFarms: res.data.length
         })
       })
       .catch(err => console.error(err));
