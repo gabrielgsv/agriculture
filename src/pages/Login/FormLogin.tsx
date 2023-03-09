75577c7f64aa8c43529eff46344c1848f5f814f7
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-03-08 22:20:28 -0300

    fix login load

--- src/pages/Login/FormLogin.tsx
+++ src/pages/Login/FormLogin.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { Button, FormLabel, Input, FormErrorMessage, FormControl, useToast } from '@chakra-ui/react'
-import { useForm } from 'react-hook-form'
-import { login } from './service';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  Button,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  FormControl,
+  useToast,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { login } from "./service";
+import { useNavigate } from "react-router-dom";
 
-import { Container } from '../../components/Layout/Container';
-import { marginFormElements } from './style';
+import { Container } from "../../components/Layout/Container";
+import { marginFormElements } from "./style";
 
 interface IFormInput {
   email: string;
@@ -13,51 +20,64 @@ interface IFormInput {
 }
 
 const FormLogin = () => {
+  const [load, setLoad] = useState(false);
   const toast = useToast();
 
-  const onSubmit = (data: IFormInput) => {
-    login(data.email, data.password, navigate, toast);
-  };
-
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors, isValid, isSubmitted } } = useForm<IFormInput>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit = (data: IFormInput) => {
+    setLoad(true);
+    login(data.email, data.password, navigate, toast, () => setLoad(false));
+  };
 
   return (
     <>
-      <form
-        onSubmit={
-          handleSubmit(onSubmit)
-        }
-      >
-        <FormControl isInvalid={isSubmitted && !isValid}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
           <Container>
-            <FormLabel htmlFor='email' className={marginFormElements}>
+            <FormLabel htmlFor="email" className={marginFormElements}>
               Email
               <Input
-                id='email'
-                type='email'
-                {...register('email', {
-                  required: 'Digite o email',
+                id="email"
+                type="email"
+                {...register("email", {
+                  required: "Digite o email",
                   pattern: {
-                    value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                    message: 'Email inválido'
-                  }
+                    value:
+                      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                    message: "Email inválido",
+                  },
                 })}
               />
-              {errors?.email?.message && <FormErrorMessage>{errors.email.message}</FormErrorMessage>}
+              {errors?.email?.message && (
+                <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+              )}
             </FormLabel>
 
-            <FormLabel htmlFor='password' className={marginFormElements}>
+            <FormLabel htmlFor="password" className={marginFormElements}>
               Senha
               <Input
-                id='password'
-                type='password'
-                {...register('password', { required: 'Digite a senha' })}
+                id="password"
+                type="password"
+                {...register("password", { required: "Digite a senha" })}
               />
-              {errors?.password?.message && <FormErrorMessage>{errors.password.message}</FormErrorMessage>}
+              {errors?.password?.message && (
+                <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+              )}
             </FormLabel>
 
-            <Button id='submit' type='submit' colorScheme='teal' className={marginFormElements}>
+            <Button
+              id="submit"
+              type="submit"
+              colorScheme="teal"
+              className={marginFormElements}
+              isLoading={load}
+            >
               Entrar
             </Button>
           </Container>
@@ -68,6 +88,3 @@ const FormLogin = () => {
 };
 
 export default FormLogin;
-
-
-
--- src/pages/Login/service.ts
+++ src/pages/Login/service.ts
@@ -1,28 +1,32 @@
-import { Auth } from '../../config/storage';
-import api from '../../services/api';
+import { Auth } from "../../config/storage";
+import api from "../../services/api";
 
 export function login(
   email: string,
   password: string,
   navigate: Function,
-  toast: any
+  toast: any,
+  callBack: Function
 ) {
-  return api.post('login', {
-    email,
-    password,
-  })
+  return api
+    .post("login", {
+      email,
+      password,
+    })
     .then((res: any) => {
       sessionStorage.setItem(Auth, res.data.accessToken);
-      navigate('/');
+      callBack();
+      navigate("/");
     })
     .catch((err: any) => {
+      callBack();
       toast({
-        title: 'Ocorreu um erro ao realizar o login!',
-        description: 'Por favor verifique seu e-mail e senha.',
-        status: 'error',
+        title: "Ocorreu um erro ao realizar o login!",
+        description: "Por favor verifique seu e-mail e senha.",
+        status: "error",
         duration: 9000,
         isClosable: true,
-      })
+      });
       console.error(err);
     });
 }
